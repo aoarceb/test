2201564b55748384aa89b037ae0f4ab70ba8268e
aoarceb <125314677+aoarceb@users.noreply.github.com>
2023-08-15 19:11:00 -0400

    Add files via upload

---
+++ Model/UserModel.php
@@ -0,0 +1,10 @@
+<?php
+require_once "C:/xampp/htdocs/TEST/Model/Database.php";
+class UserModel extends Database
+{
+    public function getUsers($limit)
+    {
+        return $this->select("SELECT * FROM users ORDER BY user_id ASC LIMIT ?", ["i", $limit]);
+    }
+}
+?>
